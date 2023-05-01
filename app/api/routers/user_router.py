43e26ec8b7a4899186ccaf7a7eb5324ff6a5e7a2
Nick <hemogram4u@yahoo.com>
2023-05-01 11:48:34 -0500

    add endpoint to subscribe users to emails

--- app/api/routers/user_router.py
+++ app/api/routers/user_router.py
@@ -2,7 +2,8 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 import app.schemas.user as schemas
-import app.middleware.crud.user_crud as crud
+import app.middleware.crud.user_crud as user_crud
+import app.middleware.crud.emailType_crud as email_type_crud
 import app.database as database
 
 user_router = APIRouter()
@@ -19,14 +20,14 @@ def get_db():
 # Endpoint to create a new user
 @user_router.post("/users", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.create_user(db, user)
+    db_user = user_crud.create_user(db, user)
     return db_user
 
 
 # Endpoint to get a user by ID
 @user_router.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+    db_user = user_crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
@@ -35,25 +36,50 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 # Endpoint to get all users with optional pagination
 @user_router.get("/users", response_model=List[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
+    users = user_crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
 # Endpoint to update a user by ID
 @user_router.put("/users/{user_id}", response_model=schemas.User)
 def update_user(user_id: int, user: schemas.User, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+    db_user = user_crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    db_user = crud.update_user(db, user_id=user_id, user=user)
+    db_user = user_crud.update_user(db, user_id=user_id, user=user)
     return db_user
 
 
 # Endpoint to delete a user by ID
 @user_router.delete("/users/{user_id}", response_model=schemas.User)
 def delete_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+    db_user = user_crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    db_user = crud.delete_user(db, user_id=user_id)
+    db_user = user_crud.delete_user(db, user_id=user_id)
     return db_user
+
+
+# Endpoint to subscribe a user to email types
+@user_router.post("/users/{user_id}/subscribe", response_model=schemas.User)
+def subscribe_user_to_email_types(
+    user_id: int, 
+    email_type_ids: List[int],
+    db: Session = Depends(get_db)
+):
+    # Check that the user exists
+    user = user_crud.get_user(db, user_id=user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    # Check that all email types exist
+    email_types = email_type_crud.get_email_types_by_ids(db, email_type_ids)
+    if not email_types:
+        raise HTTPException(status_code=404, detail="Email types not found")
+    
+    # Get the ids of the email types and put them in a list
+    found_email_ids = [email_type.id for email_type in email_types]
+
+    # Subscribe the user to the email types
+    result = user_crud.subscribe_user_to_email_types(db, user.id, found_email_ids)
+    return result
--- app/middleware/crud/emailType_crud.py
+++ app/middleware/crud/emailType_crud.py
@@ -1,10 +1,10 @@
 from sqlalchemy.orm import Session
 import app.models.models as models
 import app.schemas.email_type as schemas
+from typing import List
 
 
-# CRUD functions for email types
-
+# Create a new email type in the database
 def create_email_type(db: Session, email_type: schemas.EmailTypeCreate):
     db_email_type = models.EmailType(**email_type.dict())
     db.add(db_email_type)
@@ -13,14 +13,22 @@ def create_email_type(db: Session, email_type: schemas.EmailTypeCreate):
     return db_email_type
 
 
+# Get an email type from the database by ID
 def get_email_type(db: Session, email_type_id: int):
     return db.query(models.EmailType).filter(models.EmailType.id == email_type_id).first()
 
 
+# Get all email types from the database
 def get_email_types(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.EmailType).offset(skip).limit(limit).all()
 
 
+# Get email types from the database by a list of IDs
+def get_email_types_by_ids(db: Session, email_type_ids: List[int]):
+    return db.query(models.EmailType).filter(models.EmailType.id.in_(email_type_ids)).all()
+
+
+# Update an email type in the database
 def update_email_type(db: Session, email_type_id: int, email_type: schemas.EmailType):
     db_email_type = db.query(models.EmailType).filter(models.EmailType.id == email_type_id).first()
     for field, value in email_type:
@@ -30,6 +38,7 @@ def update_email_type(db: Session, email_type_id: int, email_type: schemas.Email
     return db_email_type
 
 
+# Delete an email type from the database
 def delete_email_type(db: Session, email_type_id: int):
     db_email_type = db.query(models.EmailType).filter(models.EmailType.id == email_type_id).first()
     db.delete(db_email_type)
