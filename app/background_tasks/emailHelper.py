6793921c9663cf1dcb5666d3d55a4de652fb1b9f
Nick <hemogram4u@yahoo.com>
2023-05-03 12:59:06 -0500

    added emailHelper class

---
+++ app/background_tasks/emailHelper.py
@@ -0,0 +1,53 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+from typing import List
+
+class EmailHelper:
+    def __init__(self, smtp_server: str, smtp_port: int, email: str, password: str):
+        self.smtp_server = smtp_server
+        self.smtp_port = smtp_port
+        self.email = email
+        self.password = password
+
+    def send_email(self, to: List[str], subject: str, body: str):
+        # Create message container
+        msg = MIMEMultipart()
+        msg['From'] = self.email
+        msg['To'] = ', '.join(to)
+        msg['Subject'] = subject
+
+        # Add body to email
+        msg.attach(MIMEText(body, 'html'))
+
+        # Open connection to SMTP server and login
+        smtp_server = smtplib.SMTP(self.smtp_server, self.smtp_port)
+        smtp_server.starttls()
+        smtp_server.login(self.email, self.password)
+
+        # Send email and close connection
+        smtp_server.sendmail(self.email, to, msg.as_string())
+        smtp_server.quit()
+
+
+    # generate an HTML email body with the specified title and message
+    # does not require instace of class or any class variables so it is static
+    @staticmethod 
+    def generate_html_body(title: str, message: str) -> str:
+        """
+        Generate an HTML email body with the specified title and message.
+        """
+        # TODO: Add CSS styling to HTML
+        # interpolate title and message into HTML template
+        html = f"""
+        <html>
+        <head>
+            <title>{title}</title>
+        </head>
+        <body>
+            <h1>{title}</h1>
+            <p>{message}</p>
+        </body>
+        </html>
+        """
+        return html
--- updates.md
+++ updates.md
@@ -8,3 +8,8 @@
 
 ## user_router.py
 - updated Endpoint to update a user by ID
+
+## background_tasks/emailHelper.py
+- added emailHelper class. 
+- need to add a lot more functionality to this class 
+- has a generate html function that needs css styling
