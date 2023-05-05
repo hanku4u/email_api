3d16e910eb9465a3474f349b8d6d5c554869c810
hanku4u <98120512+hanku4u@users.noreply.github.com>
2023-05-05 07:41:11 -0500

    Update config.py
    updated to get env vars from .env file

--- app/config.py
+++ app/config.py
@@ -1,28 +1,70 @@
-# NOTHING IS USEING THIS FILE YET
+""" Configure the fast API settings here """
+import os
+from pathlib import Path
+from functools import lru_cache
+from typing import List
+from pydantic import BaseSettings, Field
 
-from pydantic import BaseSettings
+# bucket settings
+bucket_prod = "emailAPI"
+bucket_dev = "emailAPI"
+s3_user = os.getenv("USER")
+prod_bucket = f"/mnt/{s3_user}/s3/{bucket_prod}"
+dev_bucket = f"/mnt/{s3_user}/s3/{bucket_dev}"
 
-# Define a subclass of BaseSettings to store configuration settings
+# migrate options
+migrate_opt = os.getenv("MIGRATE_OP", None)
+
+
 class Settings(BaseSettings):
-    # Set the name of the project
-    PROJECT_NAME: str = "FastAPI Example"
+    PROJECT_NAME: str = "emailAPI"
+    PROJECT_VERSION: str = "0.0.0"  # WARNING!  DO NOT TOUCH! tbump will update this automatically.
 
-    # Set the URL prefix for API endpoints
-    API_PREFIX: str = "/api"
+    # security settings
+    API_TOKEN_EXPIRE_MINUTES: int = Field(None, env="API_TOKEN_EXPIRE_MINUTES")
 
-    # Database settings
-    # Set the connection string for the SQLite database
-    SQLALCHEMY_DATABASE_URI: str = "sqlite:///./emails.sqlite"
-    
-    # Enable or disable SQLAlchemy logging
-    SQLALCHEMY_ECHO: bool = False
+    # cache settings
+    CACHE_CLEAR_TIME: int = Field(
+        None, env="CACHE_CLEAR_TIME"
+    )  # seconds to hold the cache
+    CACHE_MAX_RESULTS: int = Field(
+        None, env="CACHE_MAX_RESULTS"
+    )  # count of cache items
 
-    # Configuration for Pydantic
+    # database settings
+    DATABASE_URI: str = Field(None, env="DATABASE_URI")
+
     class Config:
-        # Load environment variables from a file named ".env"
-        env_file = ".env"
+        # choose the .env file depending on how the api is being executed
+        hostname = os.getenv("HOSTNAME")
+
+        # check if there are migration options being passed to the setting through the env
+        if migrate_opt:
+            if migrate_opt == "prod":
+                env_file =  f"{prod_bucket}/.env"
+            elif migrate_opt == "dev":
+                env_file = f"{dev_bucket}/.env"
+            else:
+                raise ValueError("wrong migrate option!")
+
+        # proceed with the normal setting checks
+        elif hostname and "vscode" in hostname:
+            # the api is being run in local/dev mode
+            env_file = Path(__file__).resolve().parent / ".env"
+
+        elif hostname == "fastapi-data.clouddev":
+            # its running on the dev cluster
+            env_file = f"{dev_bucket}/.env"
+
+        else:
+            # its running on the dev cluster
+            env_file =  f"{prod_bucket}/.env"
+
+        # .env located in the same directory as this file
+        env_file_encoding = "utf-8"
 
 
-# Create an instance of the Settings class
-# that can be used throughout the application
-settings = Settings()
+# use the cache for settings to cut down on i/o
+@lru_cache()
+def get_settings() -> BaseSettings:
+    return Settings()
